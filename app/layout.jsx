24cbcb8d8957f22bb63840a65fe59d28666fcdf5
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-09 11:38:31 +0400

    Homepage Components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@/assets/styles/globals.css"
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -13,6 +14,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -120,10 +120,10 @@ const Navbar = () => {
           {/* <!-- Right Side Menu (Logged In) --> */}
           {isLoggedIn && (
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-              <Link href="/messages" className="relative group rounded-full bg-gray-800">
+              <Link href="/messages" className="relative group ">
                 <button
                   type="button"
-                  className="relative  p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">View notifications</span>
