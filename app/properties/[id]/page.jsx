4c7858e3084b633ea1a5a7115cb7dadf4b2c9984
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-11 14:37:31 +0400

    Property Image Display

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -8,6 +8,7 @@ import { FaArrowLeft } from "react-icons/fa"
 import PropertyDetails from "@/components/PropertyDetails"
 import Spinner from "@/components/Spinner"
 import { useParams } from "next/navigation"
+import PropertyImages from "@/components/PropertyImages"
 
 const PropertyPage = () => {
   const { id } = useParams()
@@ -142,6 +143,7 @@ const PropertyPage = () => {
               </div>
             </div>
           </section>
+          <PropertyImages images={property.images} />
         </>
       )}
     </>
