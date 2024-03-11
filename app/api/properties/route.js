d40b221a7290c32e8a2f3179b9c7ac560ce1edb6
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-11 11:10:40 +0400

    Cloudinary Image Upload

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from "@/config/database"
 import Property from "@/models/Property"
 import { getSessionUser } from "@/utils/getSessionUser"
+import cloudinary from "@/config/cloudinary"
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -31,9 +32,9 @@ export const POST = async (request) => {
     const formData = await request.formData()
 
     //Access all values from amenities and images
-    const amenities = await formData.getAll("amenities")
+    const amenities = formData.getAll("amenities")
 
-    const images = await formData.getAll("images").filter((image) => image.name !== "")
+    const images = formData.getAll("images").filter((image) => image.name !== "")
 
     //Create propertyData object for database
     const propertyData = {
@@ -61,15 +62,41 @@ export const POST = async (request) => {
         phone: formData.get("seller_info.phone"),
       },
       owner: userId,
-      // images,
     }
 
-    const newProperty = await Property(propertyData)
+    //Upload images(s) to Clooudinary
 
+    // Upload image(s) to Cloudinary
+    const imageUploadPromises = []
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer()
+      const imageArray = Array.from(new Uint8Array(imageBuffer))
+      const imageData = Buffer.from(imageArray)
+
+      // Convert the image data to base64
+      const imageBase64 = imageData.toString("base64")
+
+      // Make request to upload to Cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "propertypulse001",
+        }
+      )
+
+      imageUploadPromises.push(result.secure_url)
+
+      // Wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises)
+      // Add uploaded images to the propertyData object
+      propertyData.images = uploadedImages
+    }
+
+    const newProperty = new Property(propertyData)
     await newProperty.save()
 
     return Response.redirect(`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`)
-
     // return new Response(JSON.stringify(newProperty), { status: 200 })
   } catch (error) {
     console.log(error)
