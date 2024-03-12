c4213a33106ac44dbd51fa7133b3ade500a0833a
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-12 18:46:18 +0400

    Bookmark Status & Button Toggle

--- app/api/bookmarks/check/route.js
+++ app/api/bookmarks/check/route.js
@@ -1,6 +1,5 @@
 import connectDB from "@/config/database"
 import User from "@/models/User"
-import Property from "@/models/Property"
 import { getSessionUser } from "@/utils/getSessionUser"
 
 export const dynamic = "force-dynamic"
@@ -26,23 +25,7 @@ export const POST = async (request) => {
     // Check if property is bookmarked
     let isBookmarked = user.bookmarks.includes(propertyId)
 
-    let message
-
-    if (isBookmarked) {
-      //If already bookmarked, remove it
-      user.bookmarks.pull(propertyId)
-      message = "Bookmark removed successfully"
-      isBookmarked = false
-    } else {
-      //If not bookmarked, add it
-      user.bookmarks.push(propertyId)
-      message = "Bookmark added successfully"
-      isBookmarked = true
-    }
-
-    await user.save()
-
-    return new Response(JSON.stringify({ message, isBookmarked }), { status: 200 })
+    return new Response(JSON.stringify({ isBookmarked }), { status: 200 })
   } catch (error) {
     console.log(error)
     return new Response("Something went wrong", { status: 500 })
