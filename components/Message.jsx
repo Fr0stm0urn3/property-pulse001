ca9b8bca9a009005e126fdd1998018907d831f20
Cifer0101 <l.kakabadze2020@gmail.com>
2024-03-13 21:08:22 +0400

    Delete Messages

--- components/Message.jsx
+++ components/Message.jsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify"
 
 const Message = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read)
+  const [isDeleted, setIsDeleted] = useState(false)
 
   const handleReadClick = async () => {
     try {
@@ -23,6 +24,24 @@ const Message = ({ message }) => {
       console.log(error)
       toast.error("Something went wrong")
     }
+  }
+
+  const handleDeleteClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, { method: "DELETE" })
+
+      if (res.status === 200) {
+        setIsDeleted(true)
+        toast.success("Message Deleted")
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error("Failed to delete the message")
+    }
+  }
+
+  if (isDeleted) {
+    return null
   }
 
   return (
@@ -68,7 +87,12 @@ const Message = ({ message }) => {
       >
         {isRead ? "Mark As New" : "Mark As Read"}
       </button>
-      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">Delete</button>
+      <button
+        onClick={handleDeleteClick}
+        className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md"
+      >
+        Delete
+      </button>
     </div>
   )
 }
